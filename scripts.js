3225387bb9aca0e3200316ba90053aae62ba1eed
Helga <111192958+april19833@users.noreply.github.com>
2024-07-03 00:55:26 +0300

    Update scripts.js
    Form submitted

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Welcome to Crypto Secrets for Women!");
+
+    // Пример простой интерактивности
+    document.querySelector('#contact form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Form submitted!');
+    });
 });
